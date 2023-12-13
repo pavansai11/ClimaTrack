90d46174a5e15fdf4118641379e270f68bb84d51
pavansai11 <pavansaikumar2004@gmail.com>
2023-12-13 10:49:16 +0530

    ClimaTrack

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/", function (req, res) {
